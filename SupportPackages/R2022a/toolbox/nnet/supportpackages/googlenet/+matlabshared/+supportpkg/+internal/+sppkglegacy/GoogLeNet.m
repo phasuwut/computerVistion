358a42300a06460b22de5d16c2d956fe1caee7bb
phasuwut <44774302+phasuwut@users.noreply.github.com>
2022-08-28 14:22:05 +0700

    add  file

---
+++ SupportPackages/R2022a/toolbox/nnet/supportpackages/googlenet/+matlabshared/+supportpkg/+internal/+sppkglegacy/GoogLeNet.m
@@ -0,0 +1,13 @@
+classdef GoogLeNet < matlabshared.supportpkg.internal.sppkglegacy.SupportPackageRegistryPluginBase
+% This class is used by support package infrastructure to find the legacy support_package_registry.xml
+% for the BaseCode specified
+% Type:
+% >>help matlabshared.supportpkg.internal.sppkglegacy.SupportPackageRegistryPluginBase
+% for more information.
+
+%   Copyright 2017 The MathWorks, Inc.
+
+properties(Constant)    
+	BaseCode = 'GOOGLENET';
+end
+end
---
+++ SupportPackages/R2022a/toolbox/nnet/supportpackages/googlenet/+nnet/+internal/+cnn/+supportpackages/IsGoogLeNetInstalled.m
@@ -0,0 +1,5 @@
+function IsGoogLeNetInstalled()
+% This is an empty bread-crumb function used to determine the location of
+% the GoogLeNet support package.
+
+%   Copyright 2017 The MathWorks, Inc.
