6f668a25396d39e93c758e9340cfb8092ffc8389
phasuwut <44774302+phasuwut@users.noreply.github.com>
2022-09-05 00:19:06 +0700

    [fix] delate file all

--- SupportPackages/R2022a/toolbox/nnet/supportpackages/googlenet/+nnet/+internal/+cnn/+supportpackages/IsGoogLeNetInstalled.m
+++
@@ -1,5 +0,0 @@
-function IsGoogLeNetInstalled()
-% This is an empty bread-crumb function used to determine the location of
-% the GoogLeNet support package.
-
-%   Copyright 2017 The MathWorks, Inc.
--- pyta.m
+++
@@ -1,7 +0,0 @@
-
-function c = pyta (a, b)
-% Pythagoras’ Theorem for Me
-% format is c=pyta(a,b)
-% where a and b are sides of the rectangle
-% and c^2 =a^2+b^2
-c = sqrt(a^2 + b^2)
