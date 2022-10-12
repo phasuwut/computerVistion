eaefe200db4f4bc5a235e2c57a1547adfeffd932
phasuwut <44774302+phasuwut@users.noreply.github.com>
2022-10-12 14:14:25 +0700

    [fix] delete file all

--- SupportPackages/R2022a/help/supportpkg/usbwebcams/includes/shared/scripts/topnav.js
+++
@@ -1,4 +0,0 @@
-function findDocRoot() {
-    var docRootMatches = document.location.href.match(/^.*\/help(\/releases\/R20\d\d[ab])?/);
-    return docRootMatches.length > 0 ? docRootMatches[0] : "/help";
-}
